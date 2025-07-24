f7d0d1fd13f201e05dc0c06d201b877368c189c8
Martin Terziev <m.terziev08@abv.bg>
2025-07-24 21:50:40 +0300

    Added replayability and option to show result history.

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -5,57 +5,84 @@ namespace MathGame
 {
     public class Program
     {
-        public static readonly Dictionary<string, Func<int, int, int>> _operations =
+        public static readonly Dictionary<string, Func<int, int, int>> Operations =
             new Dictionary<string, Func<int, int, int>>()
             {
                 { "add", (int x, int y) => x + y },
                 { "subtract", (int x, int y) => x - y },
                 { "multiply", (int x, int y) => x * y },
-                { "divide", (int x, int y) => x % y == 0 ? x / y : 0},
+                { "divide", (int x, int y) => x % y == 0 ? x / y : 0 },
             };
 
+        public static readonly List<string> Results = new List<string>();
+        private static string invalidDivisionMessage = "Division of entered numbers does not produce an integer!";
         static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to my Math Game!");
+            bool playAgain = true;
+            bool showHistory = false;
 
-            Console.Write("Choose an operation {Add, Subtract, Multiply, Divide}: ");
-            string operationType = Console.ReadLine().ToLower();
+            while (playAgain)
+            {
+                Console.Write("Choose an operation {Add, Subtract, Multiply, Divide}: ");
+                string operationType = Console.ReadLine().ToLower();
 
-            if (!_operations.ContainsKey(operationType))
-            {
-                Console.WriteLine("Please choose a valid operation!");
-            }
-            else
-            {
-                Console.Write("Enter 1st number (integer): ");
-                bool isFirstNumberValid = int.TryParse(Console.ReadLine(), out int firstNumber);
-                if (!isFirstNumberValid)
+                if (!Operations.TryGetValue(operationType, out Func<int, int, int> operation))
                 {
-                    Console.WriteLine("Please enter a valid number!");
+                    Console.WriteLine("Please choose a valid operation!");
                 }
                 else
                 {
-                    Console.WriteLine("Enter 2nd number: (integer)");
-                    bool isSecondNumberValid = int.TryParse(Console.ReadLine(), out int secondNumber);
-                    if (!isFirstNumberValid)
+                    Console.Write("Enter 1st number (integer): ");
+                    bool isFirstNumberValid;
+                    int firstNumber;
+                    while ((isFirstNumberValid = int.TryParse(Console.ReadLine(), out firstNumber)) == false)
                     {
-                        Console.WriteLine("Please enter a valid number");
+                        Console.WriteLine("Please enter a valid integer!");
+                        Console.Write("Enter 1st number (integer): ");
+                    }
+
+                    Console.Write("Enter 2nd number (integer): ");
+                    bool isSecondNumberValid;
+                    int secondNumber;
+                    while ((isFirstNumberValid = int.TryParse(Console.ReadLine(), out secondNumber)) == false)
+                    {
+                        Console.WriteLine("Please enter a valid integer!");
+                        Console.Write("Enter 2nd number (integer): ");
+                    }
+
+                    var result = operation(firstNumber, secondNumber);
+                    string resolution = string.Empty;
+
+                    if (operationType == "divide" && firstNumber != 0 && result == 0)
+                    {
+                        Console.WriteLine(invalidDivisionMessage);
+                        resolution = invalidDivisionMessage;
                     }
                     else
                     {
-                        var result = _operations[operationType](firstNumber, secondNumber);
-                        if (operationType == "divide" && firstNumber != 0 && result == 0)
+                        Console.WriteLine($"Result: {result}");
+                        resolution = result.ToString();
+                    }
+
+                    Results.Add(resolution);
+                    Console.Write("Would you like to play again (Yes/No): ");
+                    playAgain = Console.ReadLine().ToLower() == "yes";
+
+                    if (Results.Count > 1)
+                    {
+                        Console.Write("Would you like to see game history? (Yes/No): ");
+                        showHistory = Console.ReadLine().ToLower() == "yes";
+                        if (showHistory)
                         {
-                            Console.WriteLine("Division of selected numbers does not produce an integer!");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Result: {result}");
+                            Console.WriteLine(string.Join(Environment.NewLine, Results));
                         }
                     }
                 }
             }
-
         }
     }
 }
+
+

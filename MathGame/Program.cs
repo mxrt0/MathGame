13ee4f14dccd4e0778445a0770fe29fb330a5c73
Martin Terziev <m.terziev08@abv.bg>
2025-07-24 20:47:12 +0300

    Add project files.

---
+++ MathGame/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MathGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my Math Game!");
+
+        }
+    }
+}
